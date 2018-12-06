c406fecdaaca080916dba81e55767e378c9c00d5
priya suresh <priyasuresh@MacBook-Pro-5.local>
2018-12-05 16:51:45 -0800

    added context and modal for app

--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -203,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52542" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52325" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,8 @@
 import React from "react";
 import pf from "petfinder-client";
 import Pet from "./Pet";
+import SearchBox from "./SearchBox";
+import { Consumer } from "./SearchContext";
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -16,8 +18,16 @@ class Results extends React.Component {
     };
   }
   componentDidMount() {
+    this.search();
+  }
+  search = () => {
     petfinder.pet
-      .find({ location: "Seattle, WA", output: "full" })
+      .find({
+        location: this.props.searchParams.location,
+        animal: this.props.searchParams.animal,
+        breed: this.props.searchParams.breed,
+        output: "full"
+      })
       .then(data => {
         let pets;
         if (data.petfinder.pets && data.petfinder.pets.pet) {
@@ -33,10 +43,11 @@ class Results extends React.Component {
           pets: pets
         });
       });
-  }
+  };
   render() {
     return (
       <div className="search">
+        <SearchBox search={this.search} />
         {this.state.pets.map(pet => {
           let breed;
           if (Array.isArray(pet.breeds.breed)) {
@@ -61,5 +72,10 @@ class Results extends React.Component {
   }
 }
 
-export default Results;
-
+export default function ResultsWithContext(props) {
+  return (
+    <Consumer>
+      {context => <Results {...props} searchParams={context} />}
+    </Consumer>
+  );
+}
